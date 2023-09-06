856df7b29a54e78d9fae2040cf1809084aeb59ce
incheon94 <sonincheon94@naver.com>
2023-09-06 11:01:46 +0900

    파이참에서 커밋합니다

---
+++ 기본.py
@@ -0,0 +1,43 @@
+print("Hello, world")
+print('Hello, world')
+
+print(100)
+print(33.33)
+print(100+23)
+
+# 여기는 주석
+
+"""
+
+여기는 주석 구간 입니다 .
+
+"""
+
+# 변수를 선언하고 값을 대입
+_num = 100 # 데이터형은 값이 대입되는 순간에 결정남
+print(_num)
+num ="100"
+
+# 변수 활용
+name = "손인천"
+email = "s44851@naver.com"
+age = 12
+addr = "서울시 관악구"
+
+#출력 구간
+print(f"""
+이름 : {name}
+이메일 : {email}
+나이 : {name}
+주소 : {addr}
+""")
+
+#파이썬은 blooan 값이 대문자로 시작
+isAdult = True
+
+if isAdult : #줄 안맞추면 에러
+    print("나는 성인 입니다.")
+else:
+    print("나는 성인이 아닙니다.")
+
+
